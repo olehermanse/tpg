b8cbc0e81b4d61973e894629b0b9c85c8076d159
Ole Herman Schumacher Elgesem <ole@northern.tech>
2024-02-13 17:01:39 +0100

    Yay types
    Signed-off-by: Ole Herman Schumacher Elgesem <ole@northern.tech>

--- src/libcommon/schema.ts
+++ src/libcommon/schema.ts
@@ -1,6 +1,65 @@
 import { Schema } from "./interfaces.ts";
 
-function _validate(
+export function type_of(a: any): string {
+  if (a === true || a === false) {
+    return "boolean";
+  }
+  if (a === undefined) {
+    return "undefined";
+  }
+  if (a === null) {
+    return "null"; // NOTE: according to js, type of null is object
+  }
+  const to = typeof a;
+  if (to === "number") {
+    if (Number.isNaN(a)) {
+      return "nan";
+    }
+    return "number";
+  }
+  if (to === "string") {
+    return "string";
+  }
+  if (to === "function" && a.prototype) {
+    if (Object.getOwnPropertyDescriptor(a, "prototype")?.writable) {
+      return "function";
+    }
+    return "class " + a.name;
+  }
+  if (to === "function" && a instanceof Object) {
+    return "function";
+  }
+  if (a instanceof Object && a.constructor && a.constructor.name) {
+    return "instance " + a.constructor.name;
+  }
+  return "Unknown";
+}
+
+export function is_class(a: any, name?: string): boolean {
+  const t = type_of(a);
+  if (!t.startsWith("class ")) {
+    return false; // Not a class
+  }
+  if (name === undefined) {
+    return true; // Is a class and no name to match
+  }
+  // Match name:
+  return t === "class " + name;
+}
+
+export function is_instance(a: any, name?: string): boolean {
+  const t = type_of(a);
+  if (!t.startsWith("instance ")) {
+    return false; // Not an instance
+  }
+  if (name === undefined) {
+    return true; // Is an instance and no name to match
+  }
+  // Match name:
+  return t === "instance " + name;
+}
+
+export function validate(
   inp: Record<string, any>,
   target: any,
   schema: Schema
@@ -39,7 +98,7 @@ export function instantiate<T>(
   if (typeof inp === "string") {
     return instantiate<T>(JSON.parse(inp), target, schema);
   }
-  if (!_validate(inp, target, schema)) {
+  if (!validate(inp, target, schema)) {
     return null;
   }
   return copy(<T>inp, target, schema);
--- test/test_schema.ts
+++ test/test_schema.ts
@@ -1,4 +1,9 @@
-import { instantiate } from "../src/libcommon/schema";
+import {
+  instantiate,
+  is_class,
+  is_instance,
+  type_of,
+} from "../src/libcommon/schema";
 import { expect, test, describe } from "vitest";
 
 class FooBar {
@@ -37,5 +42,74 @@ describe("instantiate", () => {
     let s = {};
     let a = instantiate<FooBar>(s, new FooBar(), FooBar.schema);
     expect(a).toBe(null);
+  });
+});
+
+describe("type_of", () => {
+  test("a string", () => {
+    expect(type_of("foobar")).toBe("string");
+  });
+  test("null", () => {
+    expect(type_of(null)).toBe("null");
+  });
+  test("a number", () => {
+    expect(type_of(3)).toBe("number");
+  });
+  test("a class", () => {
+    expect(type_of(FooBar)).toBe("class FooBar");
+  });
+  test("an array", () => {
+    expect(type_of([])).toBe("instance Array");
+  });
+  test("an object", () => {
+    expect(type_of({})).toBe("instance Object");
+  });
+  test("an instance of a class", () => {
+    expect(type_of(new FooBar())).toBe("instance FooBar");
+  });
+  test("undefined", () => {
+    expect(type_of(undefined)).toBe("undefined");
+  });
+  test("true", () => {
+    expect(type_of(true)).toBe("boolean");
+  });
+  test("false", () => {
+    expect(type_of(false)).toBe("boolean");
+  });
+});
+
+describe("is_class", () => {
+  test("a string", () => {
+    expect(is_class("foobar")).toBe(false);
+  });
+  test("a class", () => {
+    expect(is_class(FooBar)).toBe(true);
+  });
+  test("a class with the right name", () => {
+    expect(is_class(FooBar, "FooBar")).toBe(true);
+  });
+  test("a class with the wrong name", () => {
+    expect(is_class(FooBar, "FooBarBaz")).toBe(false);
+  });
+  test("an instance", () => {
+    expect(is_class(new FooBar())).toBe(false);
+  });
+});
+
+describe("is_instance", () => {
+  test("a string", () => {
+    expect(is_instance("foobar")).toBe(false);
+  });
+  test("a class", () => {
+    expect(is_instance(FooBar)).toBe(false);
+  });
+  test("an instance", () => {
+    expect(is_instance(new FooBar())).toBe(true);
+  });
+  test("an instance with the right name", () => {
+    expect(is_instance(new FooBar(), "FooBar")).toBe(true);
+  });
+  test("an instance with the wrong name", () => {
+    expect(is_instance(new FooBar(), "FooBarBaz")).toBe(false);
   });
 });
