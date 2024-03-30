3f825ab25e4fa28bc12ffe30d0470b8968b699e7
Ole Herman Schumacher Elgesem <ole@northern.tech>
2024-03-30 04:14:02 +0100

    Implemented win checking
    Signed-off-by: Ole Herman Schumacher Elgesem <ole@northern.tech>

--- src/games/fives.ts
+++ src/games/fives.ts
@@ -7,6 +7,7 @@ type FivesSymbol = " " | "X" | "O";
 
 class Rect {
   symbol: FivesSymbol = " ";
+  highlight = false;
   x: number;
   y: number;
   w: number;
@@ -30,32 +31,9 @@ export class Fives extends BaseGame {
   board: FivesSymbol[];
   rects: Rect[];
   n: number;
-
-  constructor() {
-    super();
-    this.n = 5;
-    this.board = new Array(this.n * this.n).fill(" ");
-    this.rects = [];
-    const size = (this.height * 2) / 3;
-    const half = size / 2;
-    const w = size / this.n;
-    const h = w;
-    const center = xy(this.width / 2, this.height / 2);
-    const top_left = xy(center.x - half, center.y - half);
-
-    for (let r = 0; r < this.n; r++) {
-      for (let c = 0; c < this.n; c++) {
-        const x = top_left.x + c * w;
-        const y = top_left.y + r * h;
-        const rect = new Rect(x, y, w, h);
-        this.rects.push(rect);
-      }
-    }
-  }
-
-  get length(): number {
-    return this.n * this.n;
-  }
+  to_win: number;
+  combinations: number[][];
+  game_over: boolean;
 
   class_name(): string {
     return "Fives";
@@ -64,7 +42,158 @@ export class Fives extends BaseGame {
   schema(): Schema {
     const schema = super.base_schema();
     schema["properties"]["board"] = { type: "string", array: true };
+    schema["properties"]["n"] = { type: "number" };
+    schema["properties"]["to_win"] = { type: "number" };
     return schema;
+  }
+
+  constructor(n = 5, t = 4) {
+    super();
+    this.n = n;
+    this.to_win = t;
+    this.board = new Array(this.n * this.n).fill(" ");
+
+    this.game_over = false;
+    this.combinations = [];
+    this.rects = [];
+    this.generate_rects();
+  }
+
+  generate_rects() {
+    const size = (this.height * 2) / 3;
+    const half = size / 2;
+    const w = size / this.n;
+    const h = w;
+    const center = xy(this.width / 2, this.height / 2);
+    const top_left = xy(center.x - half, center.y - half);
+    this.rects = [];
+    for (let r = 0; r < this.n; r++) {
+      for (let c = 0; c < this.n; c++) {
+        const x = top_left.x + c * w;
+        const y = top_left.y + r * h;
+        const rect = new Rect(x, y, w, h);
+        this.rects.push(rect);
+      }
+    }
+    this.generate_combinations();
+  }
+
+  generate_combinations() {
+    const n = this.n;
+    this.combinations = [];
+    // Add rows:
+    for (let r = 0; r < n; r++) {
+      const current = [];
+      for (let c = 0; c < n; c++) {
+        const i = r * n + c;
+        current.push(i);
+      }
+      this.combinations.push(current);
+    }
+    // Add columns:
+    for (let c = 0; c < n; c++) {
+      const current = [];
+      for (let r = 0; r < n; r++) {
+        const i = r * n + c;
+        current.push(i);
+      }
+      this.combinations.push(current);
+    }
+    // Add diagonals:
+    for (let x = 0; x < n; x++) {
+      // Down and right, part 1:
+      let current = [];
+      let r = 0; // Top left corner
+      let c = x; // Moving right
+      while (r >= 0 && c >= 0 && r < n && c < n) {
+        const i = r * n + c;
+        current.push(i);
+        r += 1;
+        c += 1;
+      }
+      if (current.length >= this.to_win) {
+        this.combinations.push(current);
+      }
+      // Up and right, part 1:
+      current = [];
+      r = x; // Moving down
+      c = 0; // from top left corner
+      while (r >= 0 && c >= 0 && r < n && c < n) {
+        const i = r * n + c;
+        current.push(i);
+        r -= 1;
+        c += 1;
+      }
+      if (current.length >= this.to_win) {
+        this.combinations.push(current);
+      }
+      if (x === 0) {
+        continue;
+      }
+      // Down and right, part 2:
+      current = [];
+      r = x;
+      c = 0;
+      while (r >= 0 && c >= 0 && r < n && c < n) {
+        const i = r * n + c;
+        current.push(i);
+        r += 1;
+        c += 1;
+      }
+      if (current.length >= this.to_win) {
+        this.combinations.push(current);
+      }
+      // Up and right, part 2:
+      current = [];
+      r = this.n - 1;
+      c = x;
+      while (r >= 0 && c >= 0 && r < n && c < n) {
+        const i = r * n + c;
+        current.push(i);
+        r -= 1;
+        c += 1;
+      }
+      if (current.length >= this.to_win) {
+        this.combinations.push(current);
+      }
+    }
+  }
+
+  mark_win(streak: number[]) {
+    this.game_over = true;
+    for (const i of streak) {
+      this.rects[i].highlight = true;
+    }
+    return;
+  }
+
+  check_for_win() {
+    for (const combination of this.combinations) {
+      let streak: number[] = [];
+      let symbol = " ";
+      for (const i of combination) {
+        const current = this.board[i];
+        if (current === " ") {
+          streak = [];
+          symbol = " ";
+          continue;
+        }
+        if (current === symbol) {
+          streak.push(i);
+          if (streak.length === this.to_win) {
+            this.mark_win(streak);
+            return;
+          }
+          continue;
+        }
+        symbol = current;
+        streak = [i];
+      }
+    }
+  }
+
+  get length(): number {
+    return this.n * this.n;
   }
 
   set_square_i(i: number, symbol: FivesSymbol) {
@@ -77,6 +206,7 @@ export class Fives extends BaseGame {
     const i = row * this.n + column;
     this.board[i] = symbol;
     this.rects[i].symbol = symbol;
+    this.check_for_win();
   }
 
   refresh_rects() {
@@ -96,6 +226,7 @@ export class Fives extends BaseGame {
     if (source.board != this.board) {
       this.board = source.board;
       this.refresh_rects();
+      this.check_for_win();
     }
   }
 
@@ -109,6 +240,9 @@ export class Fives extends BaseGame {
   }
 
   mouse_click(x: number, y: number) {
+    if (this.game_over) {
+      return;
+    }
     const len = this.length;
     for (let i = 0; i < len; i++) {
       const rect = this.rects[i];
@@ -125,7 +259,7 @@ export class Fives extends BaseGame {
 
   draw(ctx: any) {
     for (const rect of this.rects) {
-      Draw.rectangle(ctx, rect.x, rect.y, rect.w, rect.h, null, "white");
+      Draw.rectangle(ctx, rect.x, rect.y, rect.w, rect.h, null, "white", 4);
       if (rect.symbol === "X") {
         Draw.line(
           ctx,
@@ -133,8 +267,8 @@ export class Fives extends BaseGame {
           rect.y + 0.2 * rect.h,
           rect.x + 0.8 * rect.w,
           rect.y + 0.8 * rect.h,
-          "white",
-          8,
+          rect.highlight ? "yellow" : "white",
+          4
         );
         Draw.line(
           ctx,
@@ -142,15 +276,23 @@ export class Fives extends BaseGame {
           rect.y + 0.2 * rect.h,
           rect.x + 0.2 * rect.w,
           rect.y + 0.8 * rect.h,
-          "white",
-          8,
+          rect.highlight ? "yellow" : "white",
+          4
         );
       }
       if (rect.symbol === "O") {
         const radius = rect.w / 2;
         const x = rect.x + radius;
         const y = rect.y + radius;
-        Draw.circle(ctx, x, y, 0.8 * radius, null, "white");
+        Draw.circle(
+          ctx,
+          x,
+          y,
+          0.8 * radius,
+          null,
+          rect.highlight ? "yellow" : "white",
+          4
+        );
       }
     }
   }
